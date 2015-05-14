e78e8e2beb8cacf2f3c9e17b285d5c2dca47c67e
trhbi <RIKAKO@Rikako-no-Air.lan>
2015-05-15 00:16:40 +0900

    練習問題

---
+++ app.js
@@ -0,0 +1,82 @@
+function skip15sec(){
+  var player  = document.querySelector("audio");
+  player.currentTime = player.currentTime + 15;
+}
+
+function onSkip15secButtonClicked(event){
+  skip15sec();
+}
+
+var skip15secButton = document.querySelector("#skip");
+skip15secButton.addEventListener("click",  onSkip15secButtonClicked);
+
+
+function back15sec(){
+  var player  = document.querySelector("audio");
+  player.currentTime = player.currentTime - 15;
+}
+
+function onBack15secButtonClicked(event){
+  back15sec();
+}
+
+var back15secButton = document.querySelector("#back");
+back15secButton.addEventListener("click",  onBack15secButtonClicked);
+
+
+function restart(){
+  var player = document.querySelector("audio");
+  player.currentTime = 0;
+}
+
+function onRestartButtonClicked(event){
+  restart();
+}
+
+var restartButton = document.querySelector("#restart");
+restartButton.addEventListener("click",  onRestartButtonClicked);
+
+
+function loop(){
+  var player = document.querySelector("audio");
+  player.loop = true;
+}
+
+function onLoopButtonClicked(event){
+  loop();
+}
+
+var loopButton = document.querySelector("#loop");
+loopButton.addEventListener("click",  onLoopButtonClicked);
+
+
+function setPlaybackRate(value){
+ var player = document.querySelector("audio");
+ player.playbackRate = value;
+}
+
+function onPlaybackRateChanged(event){
+ var rate = playbackRateControl.value;
+  setPlaybackRate(rate);
+};
+
+var playbackRateControl = 
+ document.querySelector("#playback-rate-control > input");
+ playbackRateControl.addEventListener("change",onPlaybackRateChanged);
+
+function showPlaybackRate(value){
+ var label = 
+ document.querySelector("#playback-rate-control > span");
+ label.textContent = "x " + value;
+}
+
+function setPlaybackRate(value){
+ var player = document.querySelector("audio");
+ player.playbackRate = value;
+ showPlaybackRate(value);
+}
+
+function normalSpeed(){
+  var player = document.querySelector("audio");
+  
+}
